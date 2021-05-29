6dc5d79f51386506819d6ac67e6aff3a01f579ae
aledrb <alejandradrb@gmail.com>
2021-05-29 15:52:50 +0000

    domain

--- src/app.js
+++ src/app.js
@@ -1,11 +1,17 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+function domaingenerator() {
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+  let page = [".com", ".org", ".net", ".cl"];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let j = 0; j < adj.length; j++) {
+      for (let k = 0; k < noun.length; k++) {
+        for (let l = 0; l < page.length; l++) {
+          console.log(pronoun[i] + adj[j] + noun[k] + page[l]);
+        }
+      }
+    }
+  }
+}
+console.log(domaingenerator());
